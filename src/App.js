a2d2e35779788e06572bdd6971f2013249462758
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2020-02-18 10:56:34 -0600

    implement app class

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import Products from './components/products';
 import './App.css';
 
-function App() {
-  return <div className="App"></div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+      filteredProduct: [],
+    };
+  }
+  handleAddToCart = () => {};
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Ecommerce Shopping Cart</h1>
+        <hr />
+        <div className="row">
+          <div className="col-md-8">
+            <Products
+              products={this.state.filteredProduct}
+              handleAddToCart={this.handleAddToCart}
+            />
+          </div>
+          <div className="col-md-4"></div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
