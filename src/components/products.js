465a18c2723c614a38b33a49d5d3b64150c6acb7
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2020-02-19 09:22:39 -0600

    fixed error

--- src/components/products.js
+++ src/components/products.js
@@ -5,7 +5,7 @@ class Products extends React.Component {
     const { products, handleAddToCart } = this.props;
     console.log('aaa', this.props);
     const productItems = products.map(product => (
-      <div className="col-md-8" key={product.productItems}>
+      <div className="col-md-8" key={product.id}>
         <div className="thumbnail text-center">
           <a href={`#${product.id}`} onClick={handleAddToCart}>
             <img src={`/products/${product.sku}.jpg`} alt={product.title} />
